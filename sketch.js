5097feb53e0164746faf3151810aac4d16ed1295
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-02-02 14:48:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var trex 
+//Na linha acima você está criando uma variável para o trex
+//Crie também uma variável que se chama 'trex_running' para guardar o trex correndo
+???
+
+
+function setup(){
+  //Utilize o código 'createCanvas' para criar um canva
+  createCanvas(600,200)
+
+ 
+}
+
+function draw(){
+  //Escreva uma cor em inglês para ser sua cor de fundo do jogo
+  background("???")
+  
+
+}
